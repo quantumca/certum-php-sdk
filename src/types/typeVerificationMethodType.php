edb60d7066eff6cebf431ca7577e4144aaee0546
Xiaohui Lam <xiaohui.lam@e.hexdata.cn>
2022-10-17 02:59:48 +0800

    added: `DNS_TXT_PREFIX`

--- src/types/typeVerificationMethodType.php
+++ src/types/typeVerificationMethodType.php
@@ -1,19 +1,25 @@
 <?php
+
 namespace Certum\Sdk\Types;
+
 /**
  * Partner API Library
- * 
+ *
  * @copyright Copyright (c) 2020 Asseco Data Systems SA
  * @license license.txt
  */
 
 /*
 <xs:simpleType name="verificationMethodType">
-	<xs:restriction base="xs:string">
-		<xs:enumeration value="DNS"/>
-		<xs:enumeration value="FILE"/>
-		<xs:enumeration value="ADMIN"/>
-	</xs:restriction>
+    <xs:restriction base="xs:string">
+        <xs:enumeration value="DNS"/>
+        <xs:enumeration value="DNS_TXT"/>
+        <xs:enumeration value="DNS_TXT_PREFIX"/>
+        <xs:enumeration value="DNS_CNAME"/>
+        <xs:enumeration value="DNS_CNAME_PREFIX"/>
+        <xs:enumeration value="FILE"/>
+        <xs:enumeration value="ADMIN"/>
+    </xs:restriction>
 </xs:simpleType>
 */
 
@@ -24,14 +30,18 @@ namespace Certum\Sdk\Types;
  *
  * @package types
  */
-class PartnerAPITypeVerificationMethodType {
-    
-    const DNS   = "DNS";
-    const FILE  = "FILE";
-    const ADMIN  = "ADMIN";
+class PartnerAPITypeVerificationMethodType
+{
 
-    private function __construct() {
+    const DNS               = "DNS";
+    const DNS_TXT           = "DNS_TXT";
+    const DNS_TXT_PREFIX    = "DNS_TXT_PREFIX";
+    const DNS_CNAME         = "DNS_CNAME";
+    const DNS_CNAME_PREFIX  = "DNS_CNAME_PREFIX";
+    const FILE              = "FILE";
+    const ADMIN             = "ADMIN";
+
+    private function __construct()
+    {
     }
-
-
 }
